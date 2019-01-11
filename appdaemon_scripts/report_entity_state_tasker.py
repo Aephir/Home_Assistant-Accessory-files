9e442905d6dcbe64ea70a3671c5a996cfa6d1ef8
Aephir <waldenb@gmail.com>
2019-01-11 12:09:03 +0100

    Universal state reporting to Tasker

---
+++ appdaemon_scripts/report_entity_state_tasker.py
@@ -0,0 +1,22 @@
+"""
+Report state of entity to Tasker
+"""
+
+import appdaemon.plugins.hass.hassapi as hass
+import requests
+
+class NotifyTasker(hass.Hass):
+
+    def initialize(self):
+
+    self.entities = [
+        self.args['entityID']
+        ]
+
+    for entity in self.entities:
+        self.listen_state(self.reportState, entity)
+
+    def reportState (self, entity, attribute, old, new, kwargs):
+
+        message = 'SECRET_AUTOREMOTE_URL_MESSAGE' + str(self.args['friendly_name']) + '_state_is=:=' + str(new)
+        requests.get(message)
